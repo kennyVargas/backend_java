6073061c436679c4e19c10386ba3baf77aa6cda5
kennyVargas <kenipro2@gmail.com>
2026-05-21 21:06:31 -0400

    feat: rama dev

--- src/main/java/com/example/login/LoginApplication.java
+++ src/main/java/com/example/login/LoginApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoginApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoginApplication.class, args);
+        SpringApplication.run(LoginApplication.class, args
+        );
     }
 
 }
