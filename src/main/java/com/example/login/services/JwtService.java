56f013065116e7ac167ca1b2b689e816286156cf
kennyVargas <kenipro2@gmail.com>
2026-05-25 23:05:27 -0400

    feat:  add jwt spring

---
+++ src/main/java/com/example/login/services/JwtService.java
@@ -0,0 +1,31 @@
+package com.example.login.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+    @Value("${app.jwt.secret}")
+    private String secretKey;
+
+    @Value("${app.jwt.expirationMs}")
+    private String jwtExpirationMl;
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSignInKey())
+                .build().parseClaimsJws(token)
+                .getBody();
+    }
+}
